5a51c23f17f5106d254181de474ec1346451b6b0
Allename <allename.dev@gmail.com>
2023-05-22 00:50:30 +0100

    setting up project

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,5 +1,10 @@
 const theme = {
   color: {
-
+    darkBlue: '#2b3945',
+    veryDarkBlue: '#202c37',
+    lightModeText: '#111517',
+    grey: '#858585',
+    grey_100: '#fafafa',
+    white: '#ffffff'
   }
 }
